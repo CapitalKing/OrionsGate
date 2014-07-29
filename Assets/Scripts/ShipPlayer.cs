957e98b11fa82c3db7f9a964daf1c8ed13cb7fb9
Mitchell Morehead <mitchell.morehead@yahoo.com>
2014-07-28 22:32:22 -0700

    Just threw a little bases for us we don't need to use any of it but its a nice little start.

---
+++ Assets/Scripts/ShipPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ShipPlayer : MonoBehaviour {
+	public float playerSpeed = 1;
+	public float x;
+	public float z;
+	// Use this for initialization
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () { 
+		float speed = playerSpeed;
+		//transform.position += transform.forward * Time.deltaTime * speed;
+		x = Input.GetAxis("Horizontal") * speed;
+		z = Input.GetAxis("Vertical") * speed;
+		transform.Translate(x, 0, z);
+
+	}
+}
